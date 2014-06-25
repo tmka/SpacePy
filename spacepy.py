ac504306c58244be4799ce27aaae7f45e2ff35a0
TM <Takahiro@Takahiros-MacBook-Air-2.local>
2014-06-26 02:12:26 +0900

    test for ReadCDF

--- spacepy.py
+++ spacepy.py
@@ -1,8 +1,26 @@
-
-
+import os
+os.putenv("CDF_LIB", "~/CDF/lib") #this is location which installed cdf library
+from spacepy import pycdf
 
 def main():
 	print "This is test code of SpacePy modules."
+	if not args or len(args) != 1:
+        return 0
+    CDF = args[0]
+    print "Information CDF"
+    ReadCDF(CDF)
+    print "Global Attributes"
+    OutGA(CDF)
+
+
+def ReadCDF(CDF):
+	cdf=pycdf.CDF(CDF)
+	print(cdf)
+
+def OutGA():
+	continue
+
+
 
 
 if __name__=='__main__':
